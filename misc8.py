d834b1ff02bc27a126e33c2c0950aa1e049e7304
karansingh11 <106506779+karansingh11@users.noreply.github.com>
2022-05-30 10:58:12 +0530

    Create misc8.py

---
+++ misc8.py
@@ -0,0 +1,17 @@
+# Python3 code to demonstrate
+# convert dictionary string to dictionary
+# using json.loads()
+import json
+  
+# initializing string 
+test_string = '{"Nikhil" : 1, "Akshat" : 2, "Akash" : 3}' 
+  
+# printing original string 
+print("The original string : " + str(test_string))
+  
+# using json.loads()
+# convert dictionary string to dictionary
+res = json.loads(test_string)
+  
+# print result
+print("The converted dictionary : " + str(res))
